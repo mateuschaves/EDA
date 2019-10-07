d6727d077242dde273271940fdbb94f96626b37e
Mateus Henrique <mateushenriquechaves1@gmail.com>
2019-10-07 00:36:34 -0300

    irruu

--- listaEncadeada.c
+++ listaEncadeada.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include <malloc.h>
 
+// First in First Out
+
 typedef struct node
 {
     int data;
@@ -16,14 +18,21 @@ typedef struct list
 
 List *createList();
 void push(List *list, int data);
+void printList(List *list);
 
 int main()
 {
     List *lista = createList();
 
     push(lista, 5);
-
-    printf("%d", lista->head->data);
+    push(lista, 2);
+    push(lista, 3);
+    push(lista, 1);
+    printList(lista);
+    // push(lista, 2);
+    // printf("Elemento: %d\n", lista->head->data);
+    // printf("Proximo: %d\n", lista->head->next->data);
+    // printf("Anterior: %d", lista->head->before->data);
 }
 
 List *createList()
@@ -36,11 +45,49 @@ List *createList()
     return list;
 }
 
+/*
+            1 - next = null / before = null
+
+            2 - next = 0 / 
+
+*/
+
 void push(List *list, int data)
 {
     Node *node = (Node *)malloc(sizeof(Node));
     node->data = data;
     node->next = list->head;
+    node->before = NULL;
+    if (list->head != NULL)
+        list->head->before = node;
     list->head = node;
     list->size++;
+}
+
+void printList(List *list)
+{
+    Node *pointer = list->head;
+    while (pointer != NULL)
+    {
+        printf("%d -", pointer->data);
+        if (pointer->next != NULL)
+        {
+            pointer = pointer->next;
+        }
+        else
+        {
+            break;
+        }
+    }
+    printf("\n");
+    while (1)
+    {
+        if (pointer == list->head)
+        {
+            printf("%d -", pointer->data);
+            break;
+        }
+        printf("%d -", pointer->data);
+        pointer = pointer->before;
+    }
 }
